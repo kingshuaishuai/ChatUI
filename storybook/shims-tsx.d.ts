16dc42ac073905ea81afe454b3bb3aae5aec4ac4
kingshuaishuai <ken.wang@mrs.ai>
2021-04-04 13:10:12 +0800

    #feat: 完善avatar stories

--- storybook/shims-tsx.d.ts
+++
@@ -1,7 +0,0 @@
-import { DefineComponent } from 'vue'
-
-declare global {
-  namespace JSX {
-    interface Element extends DefineComponent {}
-  }
-}
--- storybook/stories/Avatar.stories.tsx
+++ storybook/stories/Avatar.stories.tsx
@@ -5,7 +5,39 @@ import '@vue/babel-plugin-jsx'
 
 export default {
   title: 'Avatar',
-  component: Avatar
+  component: Avatar,
+  argTypes: {
+    shape: {
+      control: { type: 'radio' },
+      options: ['square', 'circle'],
+      defaultValue: 'square',
+      description: '形状',
+    },
+    size: {
+      control: { type: 'radio' },
+      options: ['sm', 'md', 'lg'],
+      defaultValue: 'md',
+      description: '大小',
+    },
+    src: {
+      control: {
+        type: 'text',
+      },
+      description: '图片地址'
+    },
+    url: {
+      control: {
+        type: 'text',
+      },
+      description: '头像链接地址：如果有设置则使用a标签包裹img，否则使用span'
+    },
+    alt: {
+      control: {
+        type: 'text',
+      },
+      description: 'img标签alt属性，图片加载失败展示信息'
+    },
+  }
 } as Meta
 
 const Template: Story<AvatarProps> = (args) => {
